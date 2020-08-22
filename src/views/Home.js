e4c3ee4ce9a5648e885df8985eda96a2ea260a0c
conilastra <cflastra@uc.cl>
2020-08-21 20:49:49 -0400

    initial config

---
+++ src/views/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+	return (
+		<main className="d-flex">
+			<header className="header">
+				<h1 className="header__title">ACADEMIA REBELDE</h1>
+				<h2 className="header__subtitle">septiembre</h2>
+			</header>
+		</main>
+	);
+};
+
+export default Home;
