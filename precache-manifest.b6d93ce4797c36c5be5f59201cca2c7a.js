4fc5e9ade6c74693fe60cdd9d8f5b8a17711c734
conilastra <cflastra@uc.cl>
2020-08-23 01:11:20 -0400

    Updates

---
+++ precache-manifest.b6d93ce4797c36c5be5f59201cca2c7a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1372206fee250a190b635d0b8ae1409c",
+    "url": "/malla-academia-rebelde/index.html"
+  },
+  {
+    "revision": "77164ef4b4d17c2359fd",
+    "url": "/malla-academia-rebelde/static/css/main.e31f4e91.chunk.css"
+  },
+  {
+    "revision": "aeceb4dc055ca1365211",
+    "url": "/malla-academia-rebelde/static/js/2.1d762f10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/malla-academia-rebelde/static/js/2.1d762f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77164ef4b4d17c2359fd",
+    "url": "/malla-academia-rebelde/static/js/main.1ccf5b97.chunk.js"
+  },
+  {
+    "revision": "9914ea5f508187a17f95",
+    "url": "/malla-academia-rebelde/static/js/runtime-main.9f70bf9f.js"
+  },
+  {
+    "revision": "5192b68a5eeb170d2506d9a25f8f7bcb",
+    "url": "/malla-academia-rebelde/static/media/irregular-circle-reverse.5192b68a.png"
+  },
+  {
+    "revision": "617e4eef344215e26bb2061751d39563",
+    "url": "/malla-academia-rebelde/static/media/irregular-circle.617e4eef.png"
+  },
+  {
+    "revision": "2ff0c839976de082898378eea6cfe782",
+    "url": "/malla-academia-rebelde/static/media/irregular-dark.2ff0c839.png"
+  },
+  {
+    "revision": "ef274747c73b650f6f9238e0779d03f6",
+    "url": "/malla-academia-rebelde/static/media/irregular-line-dark.ef274747.png"
+  },
+  {
+    "revision": "8cfd5f4d75ec9d1bd0a3a5ab421abeee",
+    "url": "/malla-academia-rebelde/static/media/irregular-line-variation.8cfd5f4d.png"
+  },
+  {
+    "revision": "7530c5cf1308363d6e1b311de31d2143",
+    "url": "/malla-academia-rebelde/static/media/irregular-line-yellow.7530c5cf.png"
+  },
+  {
+    "revision": "e98bd87d230cc0bacdd0a70fb3496549",
+    "url": "/malla-academia-rebelde/static/media/irregular-line.e98bd87d.png"
+  },
+  {
+    "revision": "b2b4a6aed4bcb0c0ef112e94f471250a",
+    "url": "/malla-academia-rebelde/static/media/irregular-paint-composed.b2b4a6ae.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/academia-rebelde/precache-manifest.2a348f22028270e7cab8d29095939220.js"
+  "/malla-academia-rebelde/precache-manifest.b6d93ce4797c36c5be5f59201cca2c7a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/academia-rebelde/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/malla-academia-rebelde/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
