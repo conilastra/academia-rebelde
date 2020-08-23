ea80251ae425021e9575813c43037bae1734e092
conilastra <cflastra@uc.cl>
2020-08-22 21:08:21 -0400

    Updates

---
+++ precache-manifest.af6d17764571d555eaf9fddb8d3cdf52.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c122dcf20f42ec824e9288ccb2623716",
+    "url": "/academia-rebelde/index.html"
+  },
+  {
+    "revision": "c35a7d2acff1fc5fe26f",
+    "url": "/academia-rebelde/static/css/main.5443aeca.chunk.css"
+  },
+  {
+    "revision": "aeceb4dc055ca1365211",
+    "url": "/academia-rebelde/static/js/2.1d762f10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/academia-rebelde/static/js/2.1d762f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c35a7d2acff1fc5fe26f",
+    "url": "/academia-rebelde/static/js/main.bf02de3e.chunk.js"
+  },
+  {
+    "revision": "682366311148a639e63a",
+    "url": "/academia-rebelde/static/js/runtime-main.1b582c15.js"
+  },
+  {
+    "revision": "617e4eef344215e26bb2061751d39563",
+    "url": "/academia-rebelde/static/media/irregular-circle.617e4eef.png"
+  },
+  {
+    "revision": "2ff0c839976de082898378eea6cfe782",
+    "url": "/academia-rebelde/static/media/irregular-dark.2ff0c839.png"
+  },
+  {
+    "revision": "ef274747c73b650f6f9238e0779d03f6",
+    "url": "/academia-rebelde/static/media/irregular-line-dark.ef274747.png"
+  },
+  {
+    "revision": "7530c5cf1308363d6e1b311de31d2143",
+    "url": "/academia-rebelde/static/media/irregular-line-yellow.7530c5cf.png"
+  },
+  {
+    "revision": "897cb12b118a84b30523142c5b99bc67",
+    "url": "/academia-rebelde/static/media/irregular-line.897cb12b.png"
+  },
+  {
+    "revision": "b2b4a6aed4bcb0c0ef112e94f471250a",
+    "url": "/academia-rebelde/static/media/irregular-paint-composed.b2b4a6ae.png"
+  }
+]);
