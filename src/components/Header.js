9288b0991b594ba84807f930217e083e5954c66b
conilastra <cflastra@uc.cl>
2020-08-23 16:04:46 -0400

    add logo in navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({ small }) => {
 	return (
-		<header className="header">
+		<header className={!small ? 'header' : 'header header--small'}>
 			<Link to="/">
 				<h1 className="header__title">ACADEMIA REBELDE</h1>
 				<h2 className="header__subtitle">septiembre</h2>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Menu from '../components/Menu';
+import { Link } from 'react-router-dom';
+import Header from './Header';
 
 const Navbar = ({ items, onClick }) => {
 	return (
 		<nav className="navbar">
+			<Header small />
 			<Menu items={items} onClick={onClick} navMode />
 		</nav>
 	);
